933d186f0ab8b4131ec2d601f4e3da6ebd3b8764
xiaoMartintin <1149044129@qq.com>
2024-07-18 20:56:17 +0800

    data-modeling on 0718

---
+++ ist-frontend/src/apis/DataModelingApi.js
@@ -0,0 +1,141 @@
+import {get, post} from './fetch.js'
+
+/**
+ * 获得所有数据模型
+ * @returns {Promise<* | void>}
+ */
+export function getDataModels() {
+    return get('/dataModelService/getAllDataModel')
+}
+
+/**
+ * 获得数据模型
+ * @param modelId
+ * @returns {Promise<*>}
+ */
+export function getDataModel(modelId) {
+    return get(`/dataModelService/getModelData/${modelId}/all/0`)
+}
+
+/**
+ * 删除数据模型
+ * @param modelId
+ * @returns {Promise<*>}
+ */
+export function deleteDataModel(modelId) {
+    return get(`/dataModelService/deleteDataModel/${modelId}`)
+}
+
+/**
+ * 导出数据模型
+ * @param modelId
+ * @returns {Promise<*>}
+ */
+export function exportDataModel(modelId) {
+    return get(`/dataModelService/getModelData/${modelId}/all/1`)
+}
+
+/**
+ * 导出数据源数据
+ * @param modelId
+ * @param sourceId
+ * @returns {Promise<*>}
+ */
+export function exportDataSource(modelId, sourceId) {
+    return get(`/dataModelService/getModelData/${modelId}/${sourceId}/1`)
+}
+
+/**
+ * 添加数据模型
+ * @param params
+ * @returns {Promise<* | void>}
+ */
+export function addDataModel(params) {
+    return post('/dataModelService/addDataModel', params)
+}
+
+/**
+ * 数据模型绑定数据源
+ * @param params
+ * @returns {Promise<*>}
+ */
+export function bindDataModel(params) {
+    return post('/dataModelService/bindDataSource', params)
+}
+
+/**
+ * 全量保存数据
+ * @param params
+ * @returns {Promise<*>}
+ */
+export function collectModelData(params) {
+    return post('/dataModelService/saveData', params)
+}
+
+/**
+ * 根据字段推荐数据模型标签
+ * @param params
+ * @returns {Promise<* | void>}
+ */
+export function querySimilarTags(params) {
+    return get('/modelSimilarityService/models/tags', params)
+}
+
+
+/**
+ * 根据字段推荐相似数据模型
+ * @param params
+ * @returns {Promise<* | void>}
+ */
+export function querySimilarModels(params) {
+    return get('/modelSimilarityService/models', params)
+}
+
+
+export function getLLMResponce(params) {
+    return post('/llmProxyService/query', params)
+}
+
+/**
+ * 添加数据源
+ * @param params
+ * @returns {Promise<* | void>}
+ */
+export function addDataSource(params) {
+    return post('/dataSourceService/addDataSource', params)
+}
+
+/**
+ * 删除数据源
+ * @param sourceId
+ * @returns {Promise<*>}
+ */
+export function deleteDataSource(sourceId) {
+    return get(`/dataSourceService/deleteDataSource/${sourceId}`)
+}
+
+/**
+ * 查询所有数据源
+ * @returns {Promise<* | void>}
+ */
+export function getAllDataSources() {
+    return get('/dataSourceService/getAllDataSource')
+}
+
+/**
+ * 查询数据源元信息
+ * @param params
+ * @returns {Promise<* | void>}
+ */
+export function getMetaFields(params) {
+    return post('/dataSourceService/getMetaFields', params)
+}
+
+/**
+ * 查询数据源所有表格
+ * @param params
+ * @returns {Promise<* | void>}
+ */
+export function getDataSourceTables(params) {
+    return post('/dataSourceService/getTables', params)
+}
---
+++ ist-frontend/src/pages/DataModeling/DataModel/methods.js
@@ -0,0 +1,77 @@
+import config from './config'
+
+export const formatVariableType = val => config.variableTypes.find(item => item.value === val)?.label
+export const formatModelType = val => config.modelTypes.find(item => item.value === val)?.label
+export const formatModelModal = val => config.modelModals.find(item => item.value === val)?.label
+export const formatRuleType = val => config.modelRules.find(item => item.value === val)?.label
+
+export function formatRuleDetail(currentRule, ruleData) {
+    if (currentRule.type === 'Unary') {
+        return '无'
+    } else if (currentRule.type === 'Binary') {
+        const relation = currentRule.relations.find(item => item.value === ruleData.type).label
+        return `${ruleData.leftFieldName} ${relation} ${ruleData.rightFieldName}`
+    } else if (currentRule.type === 'Numeric') {
+        return `${ruleData.min} ~ ${ruleData.max}`
+    }
+}
+
+export function formatMongoField(fieldName) {
+    return fieldName.replaceAll('!#@', '.')
+}
+
+export const formatDataSourceType = val => config.dataSourceType.find(item => item.value === val)?.label
+
+export const formatDataSourceTable = (dataSource) => {
+    const sourceType = formatDataSourceType(dataSource.type)
+    if (sourceType === 'PostgreSQL') {
+        return `${dataSource.schemaName}.${dataSource.tableName}`
+    } else if (sourceType === 'MongoDB') {
+        return dataSource.collectionName
+    } else if (sourceType === 'InfluxDB') {
+        return dataSource.tableName
+    } else {
+        return 'Unknown'
+    }
+}
+
+export function convertDataToCSV(data) {
+    let result = ''
+
+    if (!data || data.length === 0) {
+        return result
+    }
+
+    const columnDelimiter = ',\t'
+    const lineDelimiter = '\t\n'
+    const keys = Object.keys(data[0])
+
+    result += keys.join(columnDelimiter)
+    result += lineDelimiter
+
+    data.forEach(item => {
+        let ctr = 0
+        keys.forEach(key => {
+            if (ctr > 0) result += columnDelimiter
+            result += item[key]
+            ctr++
+        })
+        result += lineDelimiter
+    })
+
+    return result
+}
+
+export function downloadCSV(data, filename = 'exported_data.csv') {
+    const csv = convertDataToCSV(data)
+    if (!csv) return
+
+    const link = document.createElement('a')
+    link.href = "data:text/csv;charset=utf-8,\ufeff" + encodeURIComponent(csv)
+    link.download = filename
+    link.style.visibility = 'hidden'
+
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
