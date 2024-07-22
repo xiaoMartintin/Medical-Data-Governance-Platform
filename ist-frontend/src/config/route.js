797dddd3e1edea625244f3f4620c690566f5604f
Fangjiatong <fangjiatong@sjtu.edu.cn>
2024-07-22 15:22:38 +0800

    ....

--- ist-frontend/src/config/route.js
+++ ist-frontend/src/config/route.js
@@ -16,8 +16,44 @@ import DataStatisticsPage from "../pages/data-statistics/DataStatisticsPage";
 import DataDictionaryVisualizePage from "../pages/data-dictionary/DataDictionaryVisualizePage";
 import DataDictionarySearchPage from "../pages/data-dictionary/DataDictionarySearchPage";
 import DataDictionaryManagePage from "../pages/data-dictionary/DataDictionaryManagePage";
+import DataModelDetailPage from "../pages/data-model/DataModelDetailPage";
+import DataModelPage from "../pages/data-model/DataModelPage";
+import ResourceCatalogPage from "../pages/DataModeling/ResourceCatalog/ResourceCatalogPage";
 
 const routes = [
+    {
+    path: '/data-modeling',
+    label: '数据建模',
+    meta: {
+      icon: <CloudOutlined/>
+    },
+    children: [
+      {
+        path: '/resource-catalog',
+        component: <ResourceCatalogPage/>,
+        label: '资源目录管理',
+        meta: {
+          icon: <MoneyCollectOutlined/>
+        }
+      },
+      {
+        path: '/data-model',
+        component: <DataModelPage/>,
+        label: '数据模型管理',
+        meta: {
+          icon: <ShareAltOutlined/>
+        }
+      },
+      {
+        path: '/data-model/:modelId',
+        component: <DataModelDetailPage/>,
+        label: '数据模型详情',
+        meta: {
+          hidden: true
+        }
+      },
+    ]
+  },
   {
     path: '/data-asset-management',
     label: '数据资产管理',
