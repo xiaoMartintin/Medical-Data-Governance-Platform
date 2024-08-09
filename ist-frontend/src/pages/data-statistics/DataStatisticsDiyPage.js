4e11e8175929b4d379f27366412a7dbb8b2dd0c1
xiaoMartintin <1149044129@qq.com>
2024-08-09 16:49:12 +0800

    attempt of data statistics diy page

--- ist-frontend/src/pages/data-statistics/DataStatisticsDiyPage.js
+++ ist-frontend/src/pages/data-statistics/DataStatisticsDiyPage.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import DataStatisticsDiyCard from "./components/DataStatisticsDiyCard";
 import {Button, Row, Col, Pagination} from "antd";
+import { v4 as uuidv4 } from 'uuid';
 
 const DataStatisticsDiyPage = () => {
-    const [cards, setCards] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const handlePageChange = (page) => {
-        setCurrentPage(page)
-    }
-    const pageSize = 4;
+    const initialCards = JSON.parse(localStorage.getItem('cards')) || [];
+    const [cards, setCards] = useState([...initialCards]);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const handlePageChange = (page) => {
+    //     setCurrentPage(page)
+    // }
+    // const pageSize = 4;
+    //
+    // const currentCards = cards.slice((currentPage - 1) * pageSize, currentPage * pageSize)
 
-    const currentCards = cards.slice((currentPage - 1) * pageSize, currentPage * pageSize)
+    useEffect(() => {
+        localStorage.setItem('cards', JSON.stringify(cards));
+        // localStorage.clear();
+    }, [cards]);
+
     const addCard = () => {
-        setCards(prevCards => [...prevCards, {}]);
+        setCards(prevCards => [...prevCards, { id: uuidv4() }]);
+        console.log('cards:', cards)
+        console.log('current cards: ', JSON.parse(localStorage.getItem('cards')))
+    };
+
+    const deleteCard = (id) => {
+        // console.log('delete card:', id)
+        setCards(prevCards => prevCards.filter((card) => card.id !== id));
     };
 
     return (
@@ -27,9 +42,10 @@ const DataStatisticsDiyPage = () => {
             </Row>*/}
             <div style={{
                 display: 'flex',
-                flexWrap: 'wrap'}}>
-                {cards.map((card, index) => (
-                        <DataStatisticsDiyCard />
+                flexWrap: 'wrap'
+            }}>
+                {cards.map((card) => (
+                    <DataStatisticsDiyCard onDelete={deleteCard} key={card.id} id={card.id}/>
                 ))}
             </div>
         </div>
